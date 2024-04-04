0aabbac9d4fd2ae2786379647cd4c0075e157317
krmayankb <krmayankb@gmail.com>
2024-04-03 21:22:46 -0400

    ai2_setup

---
+++ scripts/ai2_setup/datacomp.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mlfoundations/datacomp.git
+cd datacomp
+bash create_env.sh
+conda activate datacomp
---
+++ scripts/ai2_setup/open_clip.sh
@@ -0,0 +1,5 @@
+conda activate datacomp
+git clone https://github.com/krmayankb/open_clip.git
+cd open_clip 
+git switch mrl_gpu 
+pip install -e .
